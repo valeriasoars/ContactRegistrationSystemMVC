fb84d452c74accc9313914fb95d99b223d38558c
Valeria Soares <valeriasoaressantos2@gmail.com>
2024-06-28 15:45:18 -0300

    Creating the Front-End

---
+++ ContactRegistrationMVC/Controllers/ContactController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ContactRegistrationMVC.Controllers
+{
+    public class ContactController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult CreateContact()
+        {
+            return View();
+        }
+
+        public IActionResult EditContact()
+        {
+            return View();
+        }
+
+        public IActionResult ConfirmationDeleteContact()
+        {
+            return View();
+        }
+    }
+}
